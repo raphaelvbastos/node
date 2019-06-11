feb006f0d65e8e2dc7e302842709adf21da1922e
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-06-11 13:16:34 -0300

    download

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var router = express.Router();
 app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.sendfile('./public/index.html');
+    console.log("PRINC");
 });
 
 app.post('/', function (req, res, next) {
@@ -30,6 +31,39 @@ app.post('/', function (req, res, next) {
         // });
     });
 });
+
+var path = require('path');
+var mime = require('mime');
+
+
+// app.get('/arquivos/:uid', function (req, res, next) {
+//     console.log("ARQUIVO");
+//     res.sendfile('./arquivos/' + req.params.uid);
+// });
+
+// app.get('/download/:uid', function(req, res){
+//     var fs = require('fs');
+
+//     var file = fs.readFileSync('./arquivos/' + req.params.uid, 'binary');
+
+
+// //   var file = fs.createReadStream("./arquivos/" + req.params.uid);
+
+// //   var file = req.params.uid;
+//   var filename = path.basename(file);
+//   var mimetype = mime.lookup(file);
+
+//   res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+//   res.setHeader('Content-type', mimetype);
+
+//   var filestream = fs.createReadStream(file);
+//   filestream.pipe(res);
+// });
+
+app.get('/download/:uid', function(req, res){
+    const file = './arquivos/' + req.params.uid;
+    res.download(file); // Set disposition and send it.
+  });
 
 var porta = process.env.PORT || 8080;
 app.listen(porta);
