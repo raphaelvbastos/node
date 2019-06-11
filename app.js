09dcd9f20e2d510a64f2c9155ae3af8d7e634c8d
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-06-11 09:18:05 -0300

    teste-node

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendfile('./public/index.html');
+});
+
+app.listen(8000);
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:8000",
+        files: ["public/**/*.*"],
+        browser: "google chrome",
+        port: 3000,
+	});
+});
+
+gulp.task('nodemon', function (cb) {
+	
+	var started = false;
+	
+	return nodemon({
+		script: 'app.js'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
