23ec39bb5ee97005b1bf9d90acdeedfc89c13181
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-06-11 11:59:57 -0300

    upload

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ var app = express();
 var router = express.Router();
 
 app.use(express.static('public'));
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendfile('./public/index.html');
+});
+
+app.post('/', function (req, res, next) {
+    var formidable = require('formidable');
+    var fs = require('fs');
+    var mv = require('mv');
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.path;
+        var newpath = './arquivos/' + files.filetoupload.name;
+
+        mv(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });
+
+
+        // fs.rename(oldpath, newpath, function (err) {
+        //     if (err) throw err;
+        //     res.write('File uploaded and moved!');
+        //     res.end();
+        // });
+    });
 });
 
 var porta = process.env.PORT || 8080;
